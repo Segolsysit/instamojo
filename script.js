01813218678e5cc40b56592be8b4eb4b335220be
AK <senthil.segolsys@gmail.com>
2023-04-11 10:22:43 +0530

    push

--- script.js
+++ script.js
@@ -9,31 +9,31 @@ const firebaseApp = firebase.initializeApp({
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
 var database = firebase.database();
-// var firebaseRef = firebase.database().ref('data')
+var firebaseRef = firebase.database().ref('data')
 
-let a = 600;
-var options = {
-  "key": "rzp_test_FjtqgTIsWBgXxV", // Enter the Key ID generated from the Dashboard
-  "amount": a * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-  "currency": "INR",
-  "name": "Acme Corp", //your business name
-  "description": "Test Transaction",
-  "image": "https://example.com/your_logo",
-  // "order_id": "rzp_test_FjtqgTIsWBgXxV", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-  "callback_url": "index.html",
-  "prefill": {
-    "name": "Gaurav Kumar", //your customer's name
-    "email": "gaurav.kumar@example.com",
-    "contact": "9000090000"
-  },
-  "notes": {
-    "address": "Razorpay Corporate Office"
-  },
-  "theme": {
-    "color": "#3399cc"
-  }
-};
-var rzp1 = new Razorpay(options);
+// let a = 600;
+// var options = {
+//   "key": "rzp_test_FjtqgTIsWBgXxV", // Enter the Key ID generated from the Dashboard
+//   "amount": a * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+//   "currency": "INR",
+//   "name": "Acme Corp", //your business name
+//   "description": "Test Transaction",
+//   "image": "https://example.com/your_logo",
+//   // "order_id": "rzp_test_FjtqgTIsWBgXxV", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+//   "callback_url": "index.html",
+//   "prefill": {
+//     "name": "Gaurav Kumar", //your customer's name
+//     "email": "gaurav.kumar@example.com",
+//     "contact": "9000090000"
+//   },
+//   "notes": {
+//     "address": "Razorpay Corporate Office"
+//   },
+//   "theme": {
+//     "color": "#3399cc"
+//   }
+// };
+// var rzp1 = new Razorpay(options);
 // rzp1.open();
 
 function signup() {
@@ -41,9 +41,15 @@ function signup() {
   const email = document.getElementById('reg_email').value;
   const password = document.getElementById('reg_pass').value;
   const username = document.getElementById('user').value
+  var firebase1Ref = firebase.database().ref('register-data')
+  var register_data ={
+    email:email,
+    username:username
+  }
   console.log(email, password);
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((result) => {
+      firebase1Ref.push(register_data)
       alert("you signed up");
       window.location.href = "login.html";
       console.log(result);
@@ -59,27 +65,27 @@ function signup() {
 
 
 function signIn() {
-  const username = document.getElementById('userlog').value;
+  // const username = document.getElementById('userlog').value;
   const email = document.getElementById('email').value;
   const password = document.getElementById('pass').value;
 
   firebase.auth().signInWithEmailAndPassword(email, password)
   .then(() => {
-      sessionStorage.setItem("user",username)
       localStorage.setItem("email", email);
       firebase.auth().onAuthStateChanged(function (user) {
+        var username = user.username;
+        sessionStorage.setItem("user",username)
           if (user) {
               var userEmail = user.email;
-              var usersRef = firebase.database().ref("emails").child("data");
+              var usersRef = firebase.database().ref("data");
               usersRef.orderByChild("email").equalTo(userEmail).once("value", function (snapshot) {
                   if (snapshot.exists()) {
                       console.log("Email exists in the database");
-                      window.location.href = "video.html"
+                      window.location.href = "course-watch.html"
 
                   } else {
                       console.log("Email does not exist in the database");
                       window.location.href = "index.html"
-
                   }
               });
           }
@@ -113,6 +119,9 @@ function forgotpassword() {
 function logout(){
   firebase.auth().signOut().then(() => {
       // Sign-out successful.
+      localStorage.removeItem("email");
+      sessionStorage.removeItem('user')
+
       window.location.href = "login.html";
     }).catch((error) => {
       // An error happened.
@@ -122,14 +131,3 @@ function logout(){
 
 // payment function
 
-function buythiscourse() {
-  const login = sessionStorage.getItem("user")
-
-  if (login === null) {
-    window.location = "login.html"
-  }
-  else {
-    rzp1.open()
-  }
-
-}
